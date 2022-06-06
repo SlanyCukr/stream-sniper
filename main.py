344c3294695b536d099da8e1178e3aa6dd560bc7
Marek Hovadík <hovadik@atlasgroup.cz>
2022-06-06 22:58:22 +0200

    Logging also prints to stdout

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ from classes.message_handler import MessageHandler
 if __name__ == '__main__':
     nickname = sys.argv[1]
 
-    logging.basicConfig(filename=f"{nickname}.log", format='%(asctime)s %(message)s', filemode='w')
-
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(f"{nickname}.log"),
+            logging.StreamHandler()
+        ]
+    )
     message_handler = MessageHandler()
     message_grabber = MessageGrabber(nickname, message_handler.update_stream_id)
     message_grabber.message_handling_fun = message_handler.handle_message
